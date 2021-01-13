0539d7283cfc047eed02fe85ab0b17c7d1eb5a3c
Patricia <patysiq@yahoo.com.br>
2021-01-13 18:43:38 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by PATRICIA S SIQUEIRA on 13/01/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
